1102070f3ed1692be06ab333b2a4addb16a7b353
mmcdevitt1997 <30372557+mmcdevitt1997@users.noreply.github.com>
2020-02-19 09:21:47 -0600

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by  Matthew McDevitt on 2/19/20.
+//  Copyright © 2020  Matthew McDevitt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
